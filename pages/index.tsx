b101b1f8b86bcf66837a8ffab7326f62d716311c
symon <simmonswalker79@gmail.com>
2024-10-18 00:37:23 +0800

    g

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,7 +118,7 @@ const index: React.FC<indexProps> = () => {
     <>
       <div id="menu-target" data-scroll-container ref={refScroll}>
         <Head>
-          <link rel="icon" href="svg/favicon.svg" />
+          <link rel="icon" href="png/Logo.png" />
           <link href="/" rel="canonical" />
           <meta name="theme-color" content="#10101A" />
           <meta
